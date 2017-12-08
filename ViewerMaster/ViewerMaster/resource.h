ff71c47e1e06d5f0e07c390becbfa383392ce009
pUrUtabU <purutabu0821@yahoo.co.jp>
2017-12-08 22:33:47 +0900

    メニューバーを更新

--- ViewerMaster/ViewerMaster/resource.h
+++ ViewerMaster/ViewerMaster/resource.h
@@ -1,14 +1,6 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by Menu.rc
+#define Option                       101
+#define Exit                    102
+#define Other                  103
+#define Version                        104
+#define Rule                       105
 
-// 新しいオブジェクトの次の既定値
-// 
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
